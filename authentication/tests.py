ac8f6dbe7ee7a4f8e9f6127fc9f75fca577047b9
santiago <stortora@softwarenatura.com>
2015-03-30 14:31:43 -0400

    Test para usuarios

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from django.contrib.auth import login
+from authentication.models import Usuario
 # Create your tests here.
+
+class CrearUsuarioTest(TestCase):
+    def setUp(self):
+        Usuario.objects.create_user('username', password='password', nombre='Juan', apellido='Perez', email='a@b.c')
+
+    def getUserAttributes(self):
+        usuario = Usuario.objects.get_by_natural_key('username')
+        self.assertEqual(usuario.username, 'username')
+        self.assertEqual(usuario.nombre, 'Juan')
+        self.assertEqual(usuario.apellido, 'Perez')
+        self.assertEqual(usuario.email, 'a@b.c')
+
+
+
